de9b50c1a4d6782cffd45a95a2c44f50f8f0c24c
John <johnwangfortune@gmail.com>
2017-07-08 17:12:33 +0900

    - Mouse hover style - not border , highlight elements

--- js/PageTaggerLib/SelectorGadget/interface.js
+++ js/PageTaggerLib/SelectorGadget/interface.js
@@ -2,12 +2,7 @@
 // Copyright (c) 2008, 2009 Kyle Maxwell
 
 function SelectorGadget() {
-  this.border_width = 5;
-  this.border_padding = 2;
-  this.b_top = null;
-  this.b_left = null;
-  this.b_right = null;
-  this.b_bottom = null;
+
   this.selected = [];
   this.rejected = [];
   this.special_mode = null;
@@ -19,117 +14,37 @@ function SelectorGadget() {
 }
 SelectorGadget.prototype = new Object();
 
-SelectorGadget.prototype.makeBorders = function(orig_elem, makeRed) {
-  this.removeBorders();
-  this.setupBorders();
-
-  if (orig_elem.parentNode)
-    var path_to_show = orig_elem.parentNode.tagName.toLowerCase() + ' ' + orig_elem.tagName.toLowerCase();
-  else
-  var path_to_show = orig_elem.tagName.toLowerCase();
-
-  var elem = jQuery(orig_elem);
-  var p = elem.offset();
-
-  var top = p.top;
-  var left = p.left;
-  var width = elem.outerWidth()
-  var height = elem.outerHeight()
-  
-  this.b_top.css('width', this.px(width + this.border_padding * 2 + this.border_width * 2)).
-             css('top', this.px(top - this.border_width - this.border_padding)).
-             css('left', this.px(left - this.border_padding - this.border_width));
-  this.b_bottom.css('width', this.px(width + this.border_padding * 2 + this.border_width * 2 - 5)).
-                css('top', this.px(top + height + this.border_padding)).
-                css('left', this.px(left - this.border_padding - this.border_width)).text(path_to_show);
-  this.b_left.css('height', this.px(height + this.border_padding * 2)).
-              css('top', this.px(top - this.border_padding)).
-              css('left', this.px(left - this.border_padding - this.border_width));
-  this.b_right.css('height', this.px(height + this.border_padding * 2)).
-               css('top', this.px(top - this.border_padding)).
-               css('left', this.px(left + width + this.border_padding));
-  
-  this.b_right.get(0).target_elem = this.b_left.get(0).target_elem = this.b_top.get(0).target_elem = this.b_bottom.get(0).target_elem = orig_elem;
-
-  if (makeRed || elem.hasClass("sg_suggested") || elem.hasClass("sg_selected")) {
-    this.b_top.addClass('sg_border_red');
-    this.b_bottom.addClass('sg_border_red');
-    this.b_left.addClass('sg_border_red');
-    this.b_right.addClass('sg_border_red');
-  } else {
-    if (this.b_top.hasClass('sg_border_red')) {
-      this.b_top.removeClass('sg_border_red');
-      this.b_bottom.removeClass('sg_border_red');
-      this.b_left.removeClass('sg_border_red');
-      this.b_right.removeClass('sg_border_red');
-    }
-  }
-  
-  this.showBorders();
-};
-
 SelectorGadget.prototype.px = function(p) {
   return p + 'px';
 };
 
-SelectorGadget.prototype.showBorders = function() {
-  this.b_top.show();
-  this.b_bottom.show();
-  this.b_left.show();
-  this.b_right.show();
+SelectorGadget.prototype.addHoverEffect = function(element){
+
+    jQuery(element).addClass('sg_hover');
+
 };
 
-SelectorGadget.prototype.removeBorders = function() {
-  if (this.b_top) {
-    this.b_top.hide();
-    this.b_bottom.hide();
-    this.b_left.hide();
-    this.b_right.hide();
-  }
-}
+SelectorGadget.prototype.removeHoverEffect = function(element){
 
-SelectorGadget.prototype.setupBorders = function() {
-  if (!this.b_top) {
-    var width = this.border_width + 'px';
-    this.b_top = jQuery('<div>').addClass('sg_border').css('height', width).hide().bind("mousedown.sg", { 'self': this }, this.sgMousedown);
-    this.b_bottom = jQuery('<div>').addClass('sg_border').addClass('sg_bottom_border').css('height', this.px(this.border_width + 6)).hide().bind("mousedown.sg", { 'self': this }, this.sgMousedown);
-    this.b_left = jQuery('<div>').addClass('sg_border').css('width', width).hide().bind("mousedown.sg", { 'self': this }, this.sgMousedown);
-    this.b_right = jQuery('<div>').addClass('sg_border').css('width', width).hide().bind("mousedown.sg", { 'self': this }, this.sgMousedown);
-    
-    this.addBorderToDom();
-  }
+    jQuery(element).removeClass('sg_hover');
+
 };
 
-SelectorGadget.prototype.addBorderToDom = function() {
-  document.body.appendChild(this.b_top.get(0));
-  document.body.appendChild(this.b_bottom.get(0));
-  document.body.appendChild(this.b_left.get(0));
-  document.body.appendChild(this.b_right.get(0));
-};
-
-SelectorGadget.prototype.removeBorderFromDom = function() {
-  if (this.b_top) {
-    this.b_top.remove();
-    this.b_bottom.remove();
-    this.b_left.remove();
-    this.b_right.remove();
-  }
-};
 
 SelectorGadget.prototype.sgMouseover = function(e) {
+  
+
   var gadget = e.data.self;
   if (gadget.unbound) return true;
   if (this == document.body || this == document.body.parentNode) return false;
   var self = jQuery(this);
   if (gadget.special_mode != 'd') { // Jump to any the first selected parent of this node.
-    var parent = gadget.firstSelectedOrSuggestedParent(this);
-    if (parent != null && parent != this)
-      gadget.makeBorders(parent, true);
-    else
-      gadget.makeBorders(this);
+
+    gadget.addHoverEffect(this);
+
   } else {
     if (!jQuery('.sg_selected', this).get(0)) {
-      gadget.makeBorders(this);
+      gadget.addHoverEffect(this);
     }
   }
   return false;
@@ -146,9 +61,11 @@ SelectorGadget.prototype.firstSelectedOrSuggestedParent = function(elem) {
 };
 
 SelectorGadget.prototype.sgMouseout = function(e) {
+  e.data.self.removeHoverEffect(this);
+
   if (e.data.self.unbound) return true;
   if (this == document.body || this == document.body.parentNode) return false;
-  e.data.self.removeBorders();
+
   return false;
 };
 
@@ -158,8 +75,8 @@ SelectorGadget.prototype.sgMousedown = function(e) {
   var elem = this;
   var w_elem = jQuery(elem);
 
-  if (w_elem.hasClass('sg_border')) {
-    // They have clicked on one of our floating borders, target the element that we are bordering.
+  if (w_elem.hasClass('sg_hover')) {
+
     elem = elem.target_elem || elem;
     w_elem = jQuery(elem);
   }
@@ -194,11 +111,10 @@ SelectorGadget.prototype.sgMousedown = function(e) {
   var prediction = gadget.prediction_helper.predictCss(gadget.selected, gadget.rejected.concat(gadget.restricted_elements));
   gadget.suggestPredicted(prediction);
   gadget.setPath(prediction);
-  gadget.removeBorders();
 
   gadget.blockClicksOn(elem);
 
-  w_elem.trigger("mouseover.sg", { 'self': gadget }); // Refresh the borders by triggering a new mouseover event.
+  w_elem.trigger("mouseover.sg", { 'self': gadget }); 
 
   return false;
 };
@@ -228,14 +144,12 @@ SelectorGadget.prototype.listenForActionKeys = function(e) {
   if (gadget.unbound) return true;
   if (e.keyCode == 16 || e.keyCode == 68) { // shift or d
     gadget.special_mode = 'd';
-    gadget.removeBorders();
   }
 };
 
 SelectorGadget.prototype.clearActionKeys = function(e) {
   var gadget = e.data.self;
   if (gadget.unbound) return true;
-  gadget.removeBorders();
   gadget.special_mode = null;
 };
 
@@ -307,7 +221,6 @@ SelectorGadget.prototype.clearSelected = function(e) {
   self.rejected = [];
   jQuery('.sg_selected').removeClass('sg_selected');
   jQuery('.sg_rejected').removeClass('sg_rejected');
-  self.removeBorders();
   self.clearSuggested();
 };
 
@@ -368,7 +281,7 @@ SelectorGadget.prototype.removeInterface = function(e) {
   this.sg_div.remove();
   this.sg_div = null;
 
-  this.removeBorderFromDom();
+  //this.removeHoverEffect(this);
 };
 
 SelectorGadget.prototype.unbind = function(e) {
@@ -388,8 +301,6 @@ SelectorGadget.prototype.rebind = function() {
   this.unbound = false;
   this.makeInterface();
   this.clearEverything();
-  this.setupBorders();
-  this.addBorderToDom();
 };
 
 SelectorGadget.prototype.randBetween = function(a, b) {
