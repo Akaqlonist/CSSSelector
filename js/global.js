ee79a599e58798f59f89ec97999c2c5302229c71
Akaqlonist <akaqlonist@gmail.com>
2022-01-09 21:34:31 -0500

    git command test

--- js/global.js
+++ js/global.js
@@ -1,5 +1,6 @@
 /*
-** file: js/global.js
-** description: global javascript library containing code for use across the entire chrome extension, for
-**              unique functionality for specific pages, create and use a separate file named "pagename.js"
-*/
+ ** file: js/global.js
+ ** description: global javascript library containing code for use across the entire chrome extension, for
+ **              unique functionality for specific pages, create and use a separate file named "pagename.js"
+ */
+// This is the global commit history
